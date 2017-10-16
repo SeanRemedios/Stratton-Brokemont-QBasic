f83faeb2fa667e8311638789d9dcef1f1b125ac6
SeanRemedios <14sr84@queensu.ca>
2017-10-16 17:02:52 -0400

    Added types.h and src

---
+++ Stratton Brokemont/src/types.h
@@ -0,0 +1,20 @@
+#ifndef __INCLUDE_types_
+#define __INCLUDE_types_
+
+#define FALSE           (0)
+#define TRUE            (1)
+
+typedef char           		Char;
+typedef unsigned char  		Uint8;
+typedef signed char		 	Int8;
+typedef unsigned short 		Uint16;
+typedef signed short   		Int16;
+typedef int            		Int;
+typedef unsigned long  		Uint32;
+typedef signed long    		Int32;
+typedef float          		Float;
+typedef signed long long 	Int64;
+typedef unsigned long long 	Uint64;
+typedef Int 				Bool;
+
+#endif
