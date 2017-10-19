98f06986e1a034ff73804d994470983610e76ab8
OneEqualsTrue <sdecimelli97@gmail.com>
2017-10-19 19:46:16 +0100

    user.h struct info

--- Stratton Brokemont/src/user.h
+++ Stratton Brokemont/src/user.h
@@ -1,11 +1,25 @@
 #ifndef __INCLUDE_USER__
 	#define __INCLUDE_USER__
 	
+#include <stdio.h>
+
 #include "types.h"
+
+typedef struct {
+	int USER;
+	FILE *VALID_PATH;
+	int *VALID_ACCTS;
+} Input;
 
 typedef enum {
 	MACHINE = 1,
 	AGENT = 2
 } Users;
+
+/*	Creates a struct of user type.
+ *	Inclides array of all valid accounts file.
+ *	
+ */
+void createStruct(Users e_user);
 
 #endif
