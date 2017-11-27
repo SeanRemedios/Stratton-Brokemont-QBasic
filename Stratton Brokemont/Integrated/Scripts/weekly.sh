74a5870638b0bd482356e25bcd11147ecb94da99
SeanRemedios <14sr84@queensu.ca>
2017-11-27 15:41:33 -0500

    Updated weekly script

--- Stratton Brokemont/Integrated/Scripts/weekly.sh
+++ Stratton Brokemont/Integrated/Scripts/weekly.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-BACKEND 	= "../BackEnd-Files/BackEnd"
-FRONTEND 	= "../FrontEnd-Files/FrontEnd"
-DAILY		= "daily.sh"
+Valid_Accounts="../BackEnd-Files/Valid_Acc.txt"
+Master_Accounts="../BackEnd-Files/Master_Acc.txt"
+Merged_Transaction="../FrontEnd-Files/Merged_Transactions.txt"
+
+#day=$(date +"%a")
+#
+#if [[ "$day" != "Sun" && "$day" != "Sat" ]]; then
+#	./DAILY
+#fi
+#
+# No need to check date, since we're only doing a simulation
+# Program would run 
+
+./daily.sh Valid_Accounts Master_Accounts Merged_Transaction 
+./daily.sh Valid_Accounts Master_Accounts Merged_Transaction
+./daily.sh Valid_Accounts Master_Accounts Merged_Transaction
+./daily.sh Valid_Accounts Master_Accounts Merged_Transaction
+./daily.sh Valid_Accounts Master_Accounts Merged_Transaction
