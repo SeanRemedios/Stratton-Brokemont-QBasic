75364d4f15848ec7da6aeba41398b4bb74e2a45c
SeanRemedios <14sr84@queensu.ca>
2017-12-01 18:38:36 -0500

    Fixed scripts and updated

--- Stratton Brokemont/Integrated/Scripts/daily.sh
+++ Stratton Brokemont/Integrated/Scripts/daily.sh
@@ -1,8 +1,31 @@
+#runs your Front End over a number of transaction sessions (at least
+#three), saving the output Transaction Summary File for each session in a separate file;
+
 #!/bin/bash
 
-BACKEND 	= "../BackEnd-Files/BackEnd"
-FRONTEND 	= "../FrontEnd-Files/FrontEnd"
-BACKINPUTPATH 	= "../BackEnd-Files/BackEnd-Input"
-BACKOUTPUTPATH 	= "../BackEnd-Files/BackEnd-Output"
-FRONTINPUTPATH 	= "../FrontEnd-Files/FrontEnd-Input"
-FRONTOUTPUTPATH = "../FrontEnd-Files/FrontEnd-Output"
+# BACKEND 	= "../BackEnd-Files/BackEnd"
+# FRONTEND 	= "../FrontEnd-Files/FrontEnd"
+# VALIDACCOUNTS = $1
+# MASTERACCOUNTS = $2
+DAY=$4
+MERGEDTRANS=$3
+MASTERACCTS=$2
+VALIDACCTS=$1
+
+PROGRAMPATH="../FrontEnd-Files/FrontEnd"
+
+echo "Daily Script"
+
+touch "../FrontEnd-Files/src/.tmp.txt" # Temporary transaction files
+
+
+TRANSPATH="../FrontEnd-Files/transactionSummaries"
+
+
+$PROGRAMPATH $VALIDACCTS $TRANSPATH/transaction1.txt < $DAY/*1.txt
+$PROGRAMPATH $VALIDACCTS $TRANSPATH/transaction2.txt < $DAY/*2.txt
+$PROGRAMPATH $VALIDACCTS $TRANSPATH/transaction3.txt < $DAY/*3.txt
+
+
+
+#________________MATT CODE__________________
--- Stratton Brokemont/Integrated/Scripts/weekly.sh
+++ Stratton Brokemont/Integrated/Scripts/weekly.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-Valid_Accounts="../BackEnd-Files/Valid_Acc.txt"
-Master_Accounts="../BackEnd-Files/Master_Acc.txt"
-Merged_Transaction="../FrontEnd-Files/Merged_Transactions.txt"
+echo "Weekly Script"
+
+Valid_Accounts		= "../BackEnd-Files/Valid_Acc.txt"
+Master_Accounts		= "../BackEnd-Files/Master_Acc.txt"
+Merged_Transactions	= "../FrontEnd-Files/Merged_Transactions.txt"
+
+
 
 #day=$(date +"%a")
 #
 #if [[ "$day" != "Sun" && "$day" != "Sat" ]]; then
-#	./DAILY
-#fi
 #
-# No need to check date, since we're only doing a simulation
-# Program would run 
-
-./daily.sh Valid_Accounts Master_Accounts Merged_Transaction 
-./daily.sh Valid_Accounts Master_Accounts Merged_Transaction
-./daily.sh Valid_Accounts Master_Accounts Merged_Transaction
-./daily.sh Valid_Accounts Master_Accounts Merged_Transaction
-./daily.sh Valid_Accounts Master_Accounts Merged_Transaction
+	for DAY in "InputTransactions" 
+	do
+		./daily.sh $Valid_Accounts $Master_Accounts $Merged_Transaction $DAY
+		# 0			1				2				3					4
+	done
+#
+#fi -- No need to check date, since we're only doing a simulation!
