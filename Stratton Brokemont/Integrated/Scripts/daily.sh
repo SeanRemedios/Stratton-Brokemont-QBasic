5eaff40e76c7442f238a7004721bd963aa59d2d2
SeanRemedios <14sr84@queensu.ca>
2017-12-01 19:25:14 -0500

    Finished the scripts and stuff

--- Stratton Brokemont/Integrated/Scripts/daily.sh
+++ Stratton Brokemont/Integrated/Scripts/daily.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
+
 #runs your Front End over a number of transaction sessions (at least
 #three), saving the output Transaction Summary File for each session in a separate file;
-
-#!/bin/bash
 
 # BACKEND 	= "../BackEnd-Files/BackEnd"
 # FRONTEND 	= "../FrontEnd-Files/FrontEnd"
@@ -12,20 +12,47 @@ MERGEDTRANS=$3
 MASTERACCTS=$2
 VALIDACCTS=$1
 
-PROGRAMPATH="../FrontEnd-Files/FrontEnd"
+# echo $0 $1 $2 $3 $4
+
+FRONTPATH="../FrontEnd-Files/FrontEnd"
+BACKPATH="../BackEnd-Files/BackEnd"
+
 
 echo "Daily Script"
 
-touch "../FrontEnd-Files/src/.tmp.txt" # Temporary transaction files
+#touch "../FrontEnd-Files/src/.tmp.txt" # Temporary transaction files
 
 
 TRANSPATH="../FrontEnd-Files/transactionSummaries"
 
 
-$PROGRAMPATH $VALIDACCTS $TRANSPATH/transaction1.txt < $DAY/*1.txt
-$PROGRAMPATH $VALIDACCTS $TRANSPATH/transaction2.txt < $DAY/*2.txt
-$PROGRAMPATH $VALIDACCTS $TRANSPATH/transaction3.txt < $DAY/*3.txt
+# Call the front end 3 times
+$FRONTPATH $VALIDACCTS $TRANSPATH/transaction1.txt < $DAY/*1.txt > /dev/null
+$FRONTPATH $VALIDACCTS $TRANSPATH/transaction2.txt < $DAY/*2.txt > /dev/null
+$FRONTPATH $VALIDACCTS $TRANSPATH/transaction3.txt < $DAY/*3.txt > /dev/null
 
 
 
-#________________MATT CODE__________________
+#________________MATT's AMAZING CODE__________________
+# Merge the transaction file
+transaction_files=$(echo $TRANSPATH*/*.txt) #get list of all transaction fles
+
+echo > $MERGEDTRANS
+touch $MERGEDTRANS
+
+for file in $transaction_files
+do
+	lines=$(wc -l < $file) #get number of lines in the file
+	lines=$((lines - 1))
+	head -n $lines $file >> $MERGEDTRANS #write all but the last time to the merged transaction file
+done
+
+echo "EOS 0000000 000 0000000 ***" >> $MERGEDTRANS #Last line of the transation file
+#________________END OF MATT's AMAZING CODE__________________
+
+# Call the back end with the merged transaction file
+$BACKPATH $MERGEDTRANS $MASTERACCTS > /dev/null
+
+# Move the accounts into the right place
+mv "masterAccounts.txt" $MASTERACCTS
+mv "validAccounts.txt" $VALIDACCTS
--- Stratton Brokemont/Integrated/Scripts/weekly.sh
+++ Stratton Brokemont/Integrated/Scripts/weekly.sh
@@ -2,9 +2,9 @@
 
 echo "Weekly Script"
 
-Valid_Accounts		= "../BackEnd-Files/Valid_Acc.txt"
-Master_Accounts		= "../BackEnd-Files/Master_Acc.txt"
-Merged_Transactions	= "../FrontEnd-Files/Merged_Transactions.txt"
+VALID="../FrontEnd-Files/validAccounts.txt"
+MASTER="../BackEnd-Files/masterAccounts.txt"
+MERGED="../BackEnd-Files/mergedTransactions.txt"
 
 
 
@@ -12,10 +12,10 @@ Merged_Transactions	= "../FrontEnd-Files/Merged_Transactions.txt"
 #
 #if [[ "$day" != "Sun" && "$day" != "Sat" ]]; then
 #
-	for DAY in "InputTransactions" 
+	for DAY in "../FrontEnd-Files/InputTransactions"/*
 	do
-		./daily.sh $Valid_Accounts $Master_Accounts $Merged_Transaction $DAY
-		# 0			1				2				3					4
+		./daily.sh $VALID $MASTER $MERGED $DAY
+		# 0			1		2		3		4
 	done
 #
 #fi -- No need to check date, since we're only doing a simulation!
